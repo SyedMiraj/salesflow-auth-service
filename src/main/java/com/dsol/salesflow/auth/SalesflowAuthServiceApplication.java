58c8e6076a58976efa88341627a7e25bd36fe444
Shahriar Miraj <smiraj2507@gmail.com>
2025-05-17 07:02:28 +0600

    first commit

---
+++ src/main/java/com/dsol/salesflow/auth/SalesflowAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsol.salesflow.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesflowAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesflowAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsol/salesflow/auth/SalesflowAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsol.salesflow.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesflowAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
